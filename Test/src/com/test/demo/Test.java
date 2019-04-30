1d81af38490734972862475a6918ce6bda52d94d
JI00611781 <JI00611781@INCHCMPC08692.TechMahindra.com>
2019-04-30 08:59:15 +0530

    First Commit

---
+++ Test/src/com/test/demo/Test.java
@@ -0,0 +1,13 @@
+package com.test.demo;
+
+public class Test {
+
+	public static void main(String[] args) {
+		for (int i = 0; i < 5; i++) {
+			System.out.println("Test Data:" + i);
+
+		}
+
+	}
+
+}
